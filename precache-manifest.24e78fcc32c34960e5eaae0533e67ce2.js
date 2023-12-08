ccd2bf8fedd69b856fce17b81817a680f51e8562
Dakshita Motwani <dakshitamotwani4@gmail.com>
2023-12-08 19:08:03 +0530

    Updates

---
+++ precache-manifest.24e78fcc32c34960e5eaae0533e67ce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46de8eeab70fd81d53b48a475e6849d",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "da8ac6218cb930797a37",
+    "url": "/TextUtils/static/css/main.182947cc.chunk.css"
+  },
+  {
+    "revision": "fcb0026ee5348c70ebc1",
+    "url": "/TextUtils/static/js/2.bdab826b.chunk.js"
+  },
+  {
+    "revision": "9ce0a34b6e7c072e8ae95df9bcbfbe1b",
+    "url": "/TextUtils/static/js/2.bdab826b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6aa5ad4c3ba0c3e292a",
+    "url": "/TextUtils/static/js/3.26d1c32d.chunk.js"
+  },
+  {
+    "revision": "da8ac6218cb930797a37",
+    "url": "/TextUtils/static/js/main.0e38732f.chunk.js"
+  },
+  {
+    "revision": "52d106dcb05c556bbe49",
+    "url": "/TextUtils/static/js/runtime-main.9353b375.js"
+  }
+]);
